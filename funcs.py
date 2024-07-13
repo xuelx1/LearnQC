dd48db2262d444484290641356b225686e4fb50e
xuelx1 <2414181961@qq.com>
2024-07-13 21:23:29 +0800

    7/13

--- funcs.py
+++ funcs.py
@@ -3,12 +3,13 @@
 '''
 from qiskit_ibm_runtime.fake_provider import FakeProvider
 from qiskit import transpile
+from qiskit_aer import AerSimulator
 
 def execute_qc(qc):
     '''
     使用模拟器执行量子程序
     '''
-    backend = FakeProvider().get_backend('fake_ourense')
+    backend = AerSimulator()
     new_qc = transpile(qc, backend)
     job = backend.run(new_qc)
     result = job.result()
