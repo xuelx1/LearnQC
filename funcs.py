699d0eac2f36cb9d4f6eb379dd3a821a0c28ad29
xuelx1 <2414181961@qq.com>
2024-07-15 16:34:17 +0800

    7/15

--- funcs.py
+++ funcs.py
@@ -22,18 +22,12 @@ def optimize_qc(qc):
     1. remove double x gate
     '''
     # remove double x gate
-    operations = list(qc.data)
     i = 0  
-    while i < len(operations) - 1:  
+    while (i < len(qc.data) - 1):  
         # check if the current and next operations are both X gates and have the same target qubit 
-        if (operations[i].name == 'x' and operations[i+1].name == 'x' and  
-                operations[i].qargs == operations[i+1].qargs):  
-            # delete these 2 gates 
-            del operations[i:i+2]   
+        if ((qc.data[i].operation.name == 'x') and (qc.data[i+1].operation.name == 'x') and (qc.data[i].qubits[0]._index == qc.data[i+1].qubits[0]._index)):  
+            del qc.data[i:i+2]   
         else:  
             # otherwise, move to the next operation  
-            i += 1
-    new_qc = QuantumCircuit.from_qasm_str(qc.qasm())  
-    new_qc.data = operations  
-      
-    return new_qc 
+            i += 1 
+    return qc 
