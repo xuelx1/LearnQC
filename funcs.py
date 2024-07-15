c801d0572fa976e7b836445cdaeb5b74e944f1fc
xuelx1 <2414181961@qq.com>
2024-07-15 15:29:07 +0800

    7/15

--- funcs.py
+++ funcs.py
@@ -1,16 +1,39 @@
 '''
-常用函数
+frequently used functions
 '''
 from qiskit_ibm_runtime.fake_provider import FakeProvider
 from qiskit import transpile
 from qiskit_aer import AerSimulator
+from qiskit import QuantumCircuit
 
 def execute_qc(qc):
     '''
-    使用模拟器执行量子程序
+    using simulator to execute qc
     '''
     backend = AerSimulator()
     new_qc = transpile(qc, backend)
     job = backend.run(new_qc)
     result = job.result()
     return result.get_counts()
+
+def optimize_qc(qc):
+    '''
+    simple optimizations of quantum circuit
+    1. remove double x gate
+    '''
+    # remove double x gate
+    operations = list(qc.data)
+    i = 0  
+    while i < len(operations) - 1:  
+        # check if the current and next operations are both X gates and have the same target qubit 
+        if (operations[i].name == 'x' and operations[i+1].name == 'x' and  
+                operations[i].qargs == operations[i+1].qargs):  
+            # delete these 2 gates 
+            del operations[i:i+2]   
+        else:  
+            # otherwise, move to the next operation  
+            i += 1
+    new_qc = QuantumCircuit.from_qasm_str(qc.qasm())  
+    new_qc.data = operations  
+      
+    return new_qc 
