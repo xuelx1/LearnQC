5450cc412e41a4cfac4877514804be334dad687f
xuelx1 <2414181961@qq.com>
2024-07-15 19:51:02 +0800

    7/15

--- funcs.py
+++ funcs.py
@@ -2,9 +2,8 @@
 frequently used functions
 '''
 from qiskit_ibm_runtime.fake_provider import FakeProvider
-from qiskit import transpile
 from qiskit_aer import AerSimulator
-from qiskit import QuantumCircuit
+from qiskit import QuantumCircuit, transpile
 
 def execute_qc(qc):
     '''
@@ -16,36 +15,3 @@ def execute_qc(qc):
     result = job.result()
     return result.get_counts()
 
-def infuncs_find_next_opt(qc, idx):
-    '''
-    find the next optimal gate on same qubit
-    '''
-    if idx >= len(qc.data):
-        raise ValueError('Index out of range')
-    opt = qc.data[idx].operation.name
-    j = 0
-    while(j+idx < len(qc.data) and len(qc.data[idx+j].qubits)==1):
-        if ((qc.data[idx+j].operation.name == opt) and 
-            (qc.data[idx].qubits[0]._index == qc.data[idx+1].qubits[0]._index)): 
-            return idx+j
-        else:
-            j += 1
-    return idx
-        
-def optimize_qc(qc):
-    '''
-    simple optimizations of quantum circuit
-    1. remove double x gate
-    '''
-    # remove double x gate
-    i = 0  
-    while (i < len(qc.data) - 1):  
-        # check if the current and next operations are both X gates and have the same target qubit 
-        if (qc.data[i].operation.name == 'x'):
-            next = infuncs_find_next_opt(qc, i)
-            print(qc.data[i].operation.name, i, next)
-            if (next > i):
-                del qc.data[i, next]
-            else:
-                i += 1
-    return qc 
